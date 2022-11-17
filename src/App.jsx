69faaff768bf49c5170e6805438fbcb2ec9f28b0
Alonso <miguelretamosoin@gmail.com>
2022-11-16 20:15:42 -0500

    Modificando State inicial

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import HeaderComponent from "./components/HeaderComponent"
 function App() {
   const [cantidad, setCantidad] = useState(10000);
 
-  console.log(cantidad)
+  function handleChange(e) {
+    console.log(+e.target.value)
+  }
 
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
@@ -12,6 +14,7 @@ function App() {
       <input
         type="range"
         className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        onChange={handleChange}
       />
     </div>
   )
